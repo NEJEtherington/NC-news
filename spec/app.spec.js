06184a1c547914999335951682f7bf17e8040e11
Nick Etherington <nicketherington@Nicks-MacBook.fritz.box>
2019-04-28 21:35:45 +0100

    remove duplicate tests for sort_by query on /api/articles

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -144,51 +144,6 @@ describe("/", () => {
         });
     });
 
-    it("GET status:200 - articles can be sorted by any valid column as a url sort_by query", () => {
-      return request
-        .get("/api/articles?sort_by=author")
-        .expect(200)
-        .then(res => {
-          expect(res.body.articles).to.be.descendingBy("author");
-        });
-    });
-
-    it("GET status:200 - articles can be sorted by any valid column as a url sort_by query", () => {
-      return request
-        .get("/api/articles?sort_by=article_id")
-        .expect(200)
-        .then(res => {
-          expect(res.body.articles).to.be.descendingBy("article_id");
-        });
-    });
-
-    it("GET status:200 - articles can be sorted by any valid column as a url sort_by query", () => {
-      return request
-        .get("/api/articles?sort_by=topic")
-        .expect(200)
-        .then(res => {
-          expect(res.body.articles).to.be.descendingBy("topic");
-        });
-    });
-
-    it("GET status:200 - articles can be sorted by any valid column as a url sort_by query", () => {
-      return request
-        .get("/api/articles?sort_by=created_at")
-        .expect(200)
-        .then(res => {
-          expect(res.body.articles).to.be.descendingBy("created_at");
-        });
-    });
-
-    it("GET ?sort_by - status:200 - articles can be sorted by any valid column as a url sort_by query", () => {
-      return request
-        .get("/api/articles?sort_by=votes")
-        .expect(200)
-        .then(res => {
-          expect(res.body.articles).to.be.descendingBy("votes");
-        });
-    });
-
     it("GET ?order - res: 200 - articles can be ordered by ascending or descending", () => {
       return request
         .get("/api/articles?sort_by=title&order=asc")
