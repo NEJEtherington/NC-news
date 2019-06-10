f2cfb4b50c17a91b435a59c805c98055aacea3d6
Nick Etherington <nicketherington@Nicks-MacBook.local>
2019-06-10 12:27:00 +0100

    single article request now returns user avatar

--- models/articles_model.js
+++ models/articles_model.js
@@ -48,12 +48,14 @@ const fetchArticleById = article_id => {
       "topic",
       "articles.body",
       "articles.created_at",
-      "articles.votes"
+      "articles.votes",
+      "articles.user_avatar"
     )
     .count({ comment_count: "comment_id" })
     .from("articles")
     .where({ "articles.article_id": article_id })
     .leftJoin("comments", "comments.article_id", "=", "articles.article_id")
+    .leftJoin("users", "users.avatar_url", "=", "articles.user_avatar")
     .groupBy("articles.article_id");
 };
 
